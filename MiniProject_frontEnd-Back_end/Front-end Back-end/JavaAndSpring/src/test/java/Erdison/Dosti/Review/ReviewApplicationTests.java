3b10543f23cfd0cfae23bc5fa8b3278433aedaf0
erdifree <123300447+erdifree@users.noreply.github.com>
2023-01-30 12:31:10 +0100

    Erdison Dosti

---
+++ MiniProject_frontEnd-Back_end/Front-end Back-end/JavaAndSpring/src/test/java/Erdison/Dosti/Review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package Erdison.Dosti.Review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
