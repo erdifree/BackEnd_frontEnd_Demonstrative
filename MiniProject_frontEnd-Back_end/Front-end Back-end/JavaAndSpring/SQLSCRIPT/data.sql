d8e8852fc46f5f2fa31f7825c0286137f235db2c
erdifree <123300447+erdifree@users.noreply.github.com>
2023-02-06 15:11:00 +0100

    Erdison Dosti

--- MiniProject_frontEnd-Back_end/Front-end Back-end/JavaAndSpring/SQLSCRIPT/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `breeds_db`.`breeds` (`description`, `image_url`, `max_life_span`, `min_life_span`, `name`, `size`, `trainability`) VALUES
- ('Il carlino è un cane da compagnia molossoide di piccola taglia brachicefalo, ovvero col muza prese è corto', 'https://media.casaegiardino.it/images/2021/02/carlino-razza-caratteristiche-e-scheda-3.jpg', '15', '12', 'Carlino', 'X-SMALL', '2'),
- ('I E un cane di costituzione robusta, raccolto e molto attivo', 'https://www.focusjunior.it/content/uploads/2019/01/labrador-5.jpg', '12', '10', 'Labrador retriever', 'MEDIUM', '5'),
- ('Il Puli è un cane molto intelligente, vivace e docile.', 'https://i.dailymail.co.uk/i/pix/scaled/2015/03/19/26C36F0400000578-0-image-a-58_1426731456276.jpg', '16', '12', 'Puli', 'MEDIUM', '2'),
- ('Cane a pelo liscio, ben proporzionato, di grande forza per la sua taglia.', 'https://www.cane-e-gatto.it/wp-content/uploads/2021/06/staffordshire-bull-terrier-1-1200.jpeg', '14', '12', 'Staffordshire Bull Terrier', 'MEDIUM', '3'),
- ('Il Border Collie è un cane di media-piccola taglia dotato di una grande resistenza e di un portamento molto elegante.', 'https://www.tuttogreen.it/wp-content/uploads/2018/06/shutterstock_715940125-700x467.jpg', '20', '12', 'Border collie', 'MEDIUM', '5'),
- ('L Akita Inu è un cane di grande taglia, molto vigoroso e dal carattere particolar', 'https://www.tecnologia-ambiente.it/wp-content/uploads/2021/05/akita2343.jpg', '12', '10', 'Akita Inu', 'LARGE', '5');
